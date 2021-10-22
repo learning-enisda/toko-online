4bc0146054bb97c13f6e43089b88c8dfe362a535
learning-enisda <learningenisda@gmail.com>
2021-10-23 06:25:46 +0800

    update web.php
    menambahkan route baru untuk contact dan memindahkan baris kode untuk route admin

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+<!-- ini adalah route untuk halaman utama admin -->
+Route::get('/wp-admin', function () {
+    return view('admin');
+});
+
+<!-- Ini adalah route untuk welcome Laravel -->
 Route::get('/', function () {
     return view('welcome');
 });
 
+<!-- ini adalah route untuk tampilan home user -->
 Route::get('/home', function () {
     return view('home');
 });
 
+<!-- ini adalah route untuk tampilan shop user -->
 Route::get('/shop', function () {
     return view('layout.main_shop');
 });
 
+<!-- ini adalah route untuk tampilan checkout user -->
 Route::get('/checkout', function () {
     return view('layout.main_checkout');
 });
 
+<!-- ini adalah route untuk tampilan cart user -->
 Route::get('/cart', function () {
     return view('layout.main_cart');
 });
 
+<!-- ini adalah route untuk tampilan blog -->
 Route::get('/blog', function () {
     return view('layout.main_blog');
 });
 
+<!-- ini adalah route untuk tampilan post -->
 Route::get('/post', function () {
     return view('layout.main_post');
 });
 
-Route::get('/wp-admin', function () {
-    return view('admin');
+<!-- ini adalah route untuk tampilan kontak -->
+Route::get('/contact', function () {
+    return view('layout.main_contact');
 });
