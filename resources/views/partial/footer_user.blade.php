902da96ecedc97db95bce1b54ad9ffe78a2d09be
learning-enisda <learningenisda@gmail.com>
2021-10-25 13:55:48 +0800

    Update footer_user.blade.php
    Co-Authored-By: jessikakindly <92999172+jessikakindly@users.noreply.github.com>
    Co-Authored-By: Samsinah Fatmawati <92993947+Inahnah@users.noreply.github.com>

--- resources/views/partial/footer_user.blade.php
+++ resources/views/partial/footer_user.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-4 col-md-6 col-sm-7">
                 <div class="footer__about">
                     <div class="footer__logo">
-                        <a href="/home"><img src="img/logo.png" alt=""></a>
+                        <a href="/home"><img src="img/toko-online.png" alt=""></a>
                     </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                     cilisis.</p>
